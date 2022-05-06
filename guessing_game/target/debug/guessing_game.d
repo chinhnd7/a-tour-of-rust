9e65f8e1dbfe754394ff5d29b7560490ccfc95d8
Manuel Sgnaolin <manuel.sgnaolin@nttdata.com>
2022-05-06 17:35:37 +0700

    commit 6/5

---
+++ guessing_game/target/debug/guessing_game.d
@@ -0,0 +1,1 @@
+D:\rust\RustLang\a-tour-of-rust\guessing_game\target\debug\guessing_game.exe: D:\rust\RustLang\a-tour-of-rust\guessing_game\src\main.rs
---
+++ guessing_game/target/rls/debug/deps/guessing_game-44d05296552bc282.d
@@ -0,0 +1,5 @@
+d:\rust\RustLang\a-tour-of-rust\guessing_game\target\rls\debug\deps\guessing_game-44d05296552bc282.rmeta: src\main.rs
+
+d:\rust\RustLang\a-tour-of-rust\guessing_game\target\rls\debug\deps\guessing_game-44d05296552bc282.d: src\main.rs
+
+src\main.rs:
